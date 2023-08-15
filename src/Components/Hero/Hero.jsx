b05745bd9d886218ec01cd8219aac74ef3204028
Kevin Johnson <harlemworld1985@gmail.com>
2023-08-15 14:46:31 +0200

    navFlexBasis

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 function Hero() {
-  return (
-    <div>
-      <p>This is a paragraph</p>
-      <h1>This is a h1</h1>
-    </div>
-  );
+  return <div>Hero</div>;
 }
 
 export default Hero;
