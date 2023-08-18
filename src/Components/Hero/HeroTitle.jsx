014a50090f5e2769fc9b98fdb621212ae7a5c25e
Kevin Johnson <harlemworld1985@gmail.com>
2023-08-18 19:09:40 +0200

    scrollZoomDisabled

---
+++ src/Components/Hero/HeroTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeroTitle() {
+  return (
+    <header className="header-Title">
+      <h1>HeroTitle</h1>
+    </header>
+  );
+}
+
+export default HeroTitle;
