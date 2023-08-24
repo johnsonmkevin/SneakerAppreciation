48ec57481574ac6ffb212a07ce077b09d31efc99
Kevin Johnson <harlemworld1985@gmail.com>
2023-08-24 10:19:28 +0200

    flex-sneaker section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import video from "../assets/Nike.mp4";
 
 export default function Hero() {
   return (
-    <div className="canvasContainer">
+    <div className="videoContainer">
       <video src={video} autoPlay loop muted />
     </div>
   );
