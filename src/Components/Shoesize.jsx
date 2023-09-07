930b7aab74874ed9047bb2405f0639c3af377201
Kevin Johnson <harlemworld1985@gmail.com>
2023-09-07 11:26:06 +0200

    sneakerStoreFullstack

--- src/Components/Shoesize.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Shoesize() {
-  return <div>Shoesize</div>;
-}
-
-export default Shoesize;
--- src/index.js
+++ src/index.js
@@ -3,25 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Layout from "./Components/Layout";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
-import store from "./redux-store";
-
-const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter>
-          <Layout>
-            <App />
-          </Layout>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
