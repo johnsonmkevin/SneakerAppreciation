25a666190cd1daf39f7a0bde8529728a3434384a
Kevin Johnson <harlemworld1985@gmail.com>
2023-09-07 11:50:35 +0200

    Layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Layout from "./Components/Layout";
+import Layout from "./Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
