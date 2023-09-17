94a886aa827503668d87560b64fa02121830b60a
Kevin Johnson <harlemworld1985@gmail.com>
2023-09-17 10:48:19 +0200

    CSS

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer-container">
       <div className="col">
-        <a href="#">FIND A STORE</a>
+        <a href="#">STORE LOCATION</a>
         <p>STUDENT DISCOUNT</p>
         <p>PROMO CODES</p>
       </div>
